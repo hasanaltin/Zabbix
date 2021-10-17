e4918beb5e9d9d22051fd01f4c3a78d646d91dec
hasanaltin <62545852+hasanaltin@users.noreply.github.com>
2021-10-17 21:57:53 +0300

    Update create_proxy.sh

--- create_proxy.sh
+++ create_proxy.sh
@@ -10,7 +10,7 @@ function safe_mkdir {
 
 #    .---------- constant part!
 #    vvvv vvvv-- the code from above
-RED='\033[0;31m'
+GREEN='\033[0;31m'
 NC='\033[0m' # No Color
 printf "${RED}You are there to install a new proxy server for Zabbix Server.${NC} \n"
 

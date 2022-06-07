50ab2f49c9d635a3392da9abc85a0d7d38d9d703
hasanaltin <62545852+hasanaltin@users.noreply.github.com>
2022-06-07 15:42:41 +0200

    Update create_proxy.sh

--- create_proxy.sh
+++ create_proxy.sh
@@ -56,7 +56,7 @@ touch zabbix/odbcinst.ini
 touch zabbix/odbc.ini
 
 
-echo "alpine-5.4-latest" >zabbix/container.version
+echo "alpine-6.0-latest" >zabbix/container.version
 
 echo "ZBX_HOSTNAME=${HOSTNAME}" >>env.list
 if [ -z "$PASSIVE_PROXY" ]; then
